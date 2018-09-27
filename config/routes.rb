fcff54bc1345b2c30505b5b58ee91e85f9a1a093
Mike Oates <oatesmi.74@gmail.com>
2018-09-27 00:14:41 -0500

    doc controller created, methods stubbed, everything working as intended

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'doc/index'
+  get 'doc/show'
+  get 'doc/new'
+  get 'doc/create'
+  get 'doc/edit'
+  get 'doc/update'
+  get 'doc/destroy'
   get 'welcome/index'
 
   root 'welcome#index'
