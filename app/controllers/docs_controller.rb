a07ca46e825f984482f37c494477d41324cbda00
Mike Oates <oatesmi.74@gmail.com>
2018-09-27 01:08:43 -0500

    edit, update, destroy methods and views created.

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -26,18 +26,25 @@ class DocsController < ApplicationController
   end
 
   def update
+    if @doc.update(doc_params)
+      redirect_to @doc
+    else
+      render 'edit'
+    end
   end
 
   def destroy
+    @doc.destroy
+    redirect_to docs_path
   end
 
   private
 
-  def find_doc
-    @doc = Doc.find(params[:id])
-  end
+    def find_doc
+      @doc = Doc.find(params[:id])
+    end
 
-  def doc_params
-    params.require(:doc).permit(:title, :content)
-  end
+    def doc_params
+      params.require(:doc).permit(:title, :content)
+    end
 end
